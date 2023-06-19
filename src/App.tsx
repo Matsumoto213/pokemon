f07b753efd81ba77c1f593a804f0e0804d968dcc
Matsumoto213 <matsumoto2171205@gmail.com>
2023-06-19 22:05:05 +0900

    ブランチ確認

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,7 @@ const PokemonList: React.FC = () => {
       <ul>
         {pokemonData.map((pokemon) => (
           <li key={pokemon.name}>
+            <p>おはよう</p>
             {pokemon.name}
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
           </li>
