7d7b179940e7efe0e0e04f020173b6ae7868cee0
Matsumoto213 <matsumoto2171205@gmail.com>
2023-06-24 11:13:40 +0900

    検索ボタンなどを配置して見た目を整えましょう #7

--- src/components/PokemonFilter.tsx
+++ src/components/PokemonFilter.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import { FaSearch } from "react-icons/fa"
+import { Select, Option } from "@material-tailwind/react";
 
 const PokemonFilter: React.FC = () => {
     const [generation, setGeneration] = useState('');
     const [type, setType] = useState('');
     const [count, setCount] = useState('');
+
+    const handleChangeGeneration = (event:React.ChangeEvent<HTMLInputElement>): void =>setGeneration(event.target.value);
+    const handleChangeType = (event:React.ChangeEvent<HTMLInputElement>): void => setType(event.target.value);
+    const handleChangeCount = (event:React.ChangeEvent<HTMLInputElement>): void => setCount(event.target.value);
+
 
     const handleClick = () => {
         // ここで検索処理を実行
@@ -14,54 +20,57 @@ const PokemonFilter: React.FC = () => {
     return (
         <div className="PokemonFilter">
             <div className="filter-wrapper">
-              <select 
-                  value={generation} 
-                  onChange={(e) => setGeneration(e.target.value)}
+              <Select 
+                value={generation}
+                onChange={(event) => handleChangeGeneration(event)}
+                variant="outlined" label="世代"
               >
-                <option value="">世代</option>
-                <option value="1">第1世代</option>
-                <option value="2">第2世代</option>
-                <option value="3">第3世代</option>
-                <option value="4">第4世代</option>
-                <option value="5">第5世代</option>
-                <option value="6">第6世代</option>
-                <option value="7">第7世代</option>
-              </select>
-              <select 
+                <Option value="">なし</Option>
+                <Option value="1">第1世代</Option>
+                <Option value="2">第2世代</Option>
+                <Option value="3">第3世代</Option>
+                <Option value="4">第4世代</Option>
+                <Option value="5">第5世代</Option>
+                <Option value="6">第6世代</Option>
+                <Option value="7">第7世代</Option>
+              </Select>
+              <Select 
                   value={type} 
-                  onChange={(e) => setType(e.target.value)}
+                  onChange={handleChangeType}
+                  variant="outlined" label="タイプ"
               >
-                <option value="">タイプ</option>
-                <option value="normal">ノーマル</option>
-                <option value="fire">ほのお</option>
-                <option value="water">みず</option>
-                <option value="electricity">でんき</option>
-                <option value="grass">くさ</option>
-                <option value="ice">こおり</option>
-                <option value="fighting">かくとう</option>
-                <option value="poison">どく</option>
-                <option value="ground">じめん</option>
-                <option value="flying">ひこう</option>
+                <Option value="">なし</Option>
+                <Option value="normal">ノーマル</Option>
+                <Option value="fire">ほのお</Option>
+                <Option value="water">みず</Option>
+                <Option value="electricity">でんき</Option>
+                <Option value="grass">くさ</Option>
+                <Option value="ice">こおり</Option>
+                <Option value="fighting">かくとう</Option>
+                <Option value="poison">どく</Option>
+                <Option value="ground">じめん</Option>
+                <Option value="flying">ひこう</Option>
 
-                <option value="esper">エスパー</option>
-                <option value="insect">むし</option>
-                <option value="rock">いわ</option>
-                <option value="ghost">ゴースト</option>
-                <option value="dragon">ドラゴン</option>
-                <option value="steel">はがね</option>
-              </select>
-              <select 
+                <Option value="esper">エスパー</Option>
+                <Option value="insect">むし</Option>
+                <Option value="rock">いわ</Option>
+                <Option value="ghost">ゴースト</Option>
+                <Option value="dragon">ドラゴン</Option>
+                <Option value="steel">はがね</Option>
+              </Select>
+              <Select 
                   value={count} 
-                  onChange={(e) => setCount(e.target.value)}
+                  onChange={handleChangeCount}
+                  variant="outlined" label="件数"
               >
-                <option value="">件数</option>
-                <option value="5">5件</option>
-                <option value="10">10件</option>
-                <option value="20">20件</option>
-                <option value="30">30件</option>
-                <option value="50">50件</option>
-                <option value="100">100件</option>
-              </select>
+                <Option value="">件数</Option>
+                <Option value="5">5件</Option>
+                <Option value="10">10件</Option>
+                <Option value="20">20件</Option>
+                <Option value="30">30件</Option>
+                <Option value="50">50件</Option>
+                <Option value="100">100件</Option>
+              </Select>
               <button className="search-button" onClick={handleClick}>
                 <FaSearch />
               </button>
